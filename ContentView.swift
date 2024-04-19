745ad10c2bc678634070d2994a210eb30a0fb2b6
Christian <Cdzhokov6094@stu.d214.org>
2024-04-19 14:48:33 -0500

    created texts showing title of app and slogan

--- ContentView.swift
+++ ContentView.swift
@@ -2,6 +2,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        Text("Leaf-Budget Smartly")
+        Text("Budget Without The Worry of Overspending")
         NavigationView {
          
                 HistoryView()
