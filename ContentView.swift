de96817517c5bb2d42c5abce789f2b67fa2a2dcb
Allan Sulev <asulev7401@stu.d214.org>
2024-04-19 14:59:05 -0500

    Added Home Image

--- ContentView.swift
+++ ContentView.swift
@@ -2,6 +2,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        Image(systemName: "house.circle")
+                    .scaleEffect(CGSize(width: 5, height: 5))
+                    .offset(x: 0, y: 390)
         Text("Leaf-Budget Smartly")
         Text("Budget Without The Worry of Overspending")
         NavigationView {
