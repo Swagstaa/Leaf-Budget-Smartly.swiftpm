63e0f3ccfcd81ab181e6b4e1593165355f4c2b3c
Karim <kramirezchina7337@stu.d214.org>
2024-04-19 14:59:24 -0500

    Added text rto welceom

--- ContentView.swift
+++ ContentView.swift
@@ -4,6 +4,7 @@ struct ContentView: View {
     var body: some View {
         Text("Leaf-Budget Smartly")
         Text("Budget Without The Worry of Overspending")
+        Text("Welcome")
         NavigationView {
          
                 HistoryView()
