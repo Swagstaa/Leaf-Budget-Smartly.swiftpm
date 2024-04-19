e4dbccf8983eb4dde69545f032cf85c88c038c71
Allan Sulev <asulev7401@stu.d214.org>
2024-04-19 16:28:54 -0500

    added button for tyler

--- ContentView.swift
+++ ContentView.swift
@@ -2,16 +2,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Image(systemName: "house.circle")
-                    .scaleEffect(CGSize(width: 5, height: 5))
-                    .offset(x: 0, y: 390)
-        Text("Leaf-Budget Smartly")
-        Text("Budget Without The Worry of Overspending")
-        Text("Welcome")
-        NavigationView {
-         
-                HistoryView()
+        VStack(spacing:60){
             
+                Text("Leaf-Budget Smartly")
+                Text("Budget Without The Worry of Overspending")
+                Text("Welcome")
+                
+                Button("Begin"){
+                    
+                }
+                Image(systemName: "house.circle")
+                    .scaleEffect(CGSize(width: 5, height: 5))
+            }
         }
     }
-}
+
